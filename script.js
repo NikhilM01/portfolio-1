79ecbc01a47bd1b8d46f2b4696209f91940e4a6c
NikhilM01 <nikhilmuneshwar01@gmail.com>
2024-03-03 17:09:19 +0530

    custom-cursor

--- script.js
+++ script.js
@@ -148,32 +148,13 @@ let workInfoItems = document.querySelectorAll('.work__photo-item');
 
   (function () {
 
-    const link = document.querySelectorAll('nav > .hover-this');
-    const cursor = document.querySelector('.cursor');
+    const customCursor = document.querySelector('.cursor');
 
-    const animateit = function (e) {
-          const span = this.querySelector('span');
-          const { offsetX: x, offsetY: y } = e,
-          { offsetWidth: width, offsetHeight: height } = this,
-
-          move = 25,
-          xMove = x / width * (move * 2) - move,
-          yMove = y / height * (move * 2) - move;
-
-          span.style.transform = `translate(${xMove}px, ${yMove}px)`;
-
-          if (e.type === 'mouseleave') span.style.transform = '';
-    };
-
-    const editCursor = e => {
-          const { clientX: x, clientY: y } = e;
-          cursor.style.left = x + 'px';
-          cursor.style.top = y + 'px';
-    };
-
-    link.forEach(b => b.addEventListener('mousemove', animateit));
-    link.forEach(b => b.addEventListener('mouseleave', animateit));
-    window.addEventListener('mousemove', editCursor);
+    document.addEventListener('mousemove', (e) => {
+        // Update the position of the custom cursor based on mouse movement
+        customCursor.style.left = `${e.clientX}px`;
+        customCursor.style.top = `${e.clientY}px`;
+    });
 
 })();
 
